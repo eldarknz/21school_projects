7e51eb8e263628ffb837602527306889b5d2ae5d
Horace Kayla <hkayla@mi-i6.21-school.ru>
2020-02-09 14:14:18 +0300

    work

---
+++ fillit/fillit_new_3/src/checker.c
@@ -0,0 +1,76 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   checker.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hkayla <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/02/07 11:29:33 by hkayla            #+#    #+#             */
+/*   Updated: 2020/02/07 11:29:35 by hkayla           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "fillit.h"
+
+int		neighbor_counter(char *buf)
+{
+	int i;
+	int count;
+
+	i = 0;
+	count = 0;
+	while (i < 19)
+	{
+		if (buf[i] == '#')
+		{
+			if (i + 1 <= 18 && buf[i + 1] == '#')
+				count++;
+			if (i - 1 >= 0 && buf[i - 1] == '#')
+				count++;
+			if (i + 5 <= 18 && buf[i + 5] == '#')
+				count++;
+			if (i - 5 >= 0 && buf[i - 5] == '#')
+				count++;
+		}
+		i++;
+	}
+	return (count);
+}
+
+int		character_counter(char *buf)
+{
+	int i;
+	int count;
+
+	count = 0;
+	i = 0;
+	while (i < 19)
+	{
+		if (buf[i] && buf[i] != '.' && buf[i] != '#' && buf[i] != '\n')
+			return (0);
+		if (buf[i] == '\n' && (i + 1) % 5 != 0)
+			return (0);
+		if (buf[i] == '#')
+			count++;
+		i++;
+	}
+	if (!buf[i] || buf[i] != '\n')
+		return (0);
+	return (count);
+}
+
+int		check_figure(char *buf, int size)
+{
+	int i;
+
+	i = 0;
+	while (i <= size)
+	{
+		if (character_counter(buf + i) != 4)
+			return (0);
+		if (neighbor_counter(buf + i) != 6 && neighbor_counter(buf + i) != 8)
+			return (0);
+		i += 21;
+	}
+	return (1);
+}
---
+++ fillit/fillit_new_3/src/fillit.h
@@ -0,0 +1,55 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   fillit.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hkayla <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/02/03 14:30:37 by hkayla            #+#    #+#             */
+/*   Updated: 2020/02/03 14:30:41 by hkayla           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FILLIT_H
+# define FILLIT_H
+
+# include <unistd.h>
+# include <fcntl.h>
+# include "../libft/libft.h"
+
+typedef struct	s_figure
+{
+	int				coord_x[4];
+	int				coord_y[4];
+	char			figuresymbol;
+	int				offset_x;
+	int				offset_y;
+	struct s_figure	*next;
+}				t_figure;
+
+typedef struct	s_map
+{
+	char		**array;
+}				t_map;
+
+t_figure		*read_data(char *filename);
+int				check_figure(char *buf, int size);
+int				character_counter(char *buf);
+int				neighbor_counter(char *buf);
+t_figure		*create_figure(char *buf, char figuresymbol);
+t_figure		*create_figure_list(char *buf, int size);
+t_figure		*shift_top_left(t_figure *figure);
+void			shift_axis(t_figure *figure, int n, char axis);
+void			solve(t_figure *figurelist);
+int				get_map_size(int num);
+size_t			figure_counter(t_figure *figurelist);
+int				build_map(t_map *map, t_figure *figurelist, int map_size);
+int				is_inside(t_figure *figure, int map_size, char axis);
+int				intersection(t_map *map, t_figure *figure);
+void			insert_figure(t_figure *figure, t_map *map, char letter);
+t_map			*new_map(int size);
+void			print_map(t_map *map, int size);
+void			free_list(t_figure *list);
+void			free_map(t_map *map, int map_size);
+
+#endif
